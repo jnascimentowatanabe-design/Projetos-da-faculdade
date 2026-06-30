b55b87cc1bb106cd423eb78833f218173ac43f7d
Júlio <jnascimentowatanabe@gmail.com>
2026-06-30 18:53:40 -0300

    adding new methods

--- Portifólio Java/Project_1/BookApp/PagesOfBook.java
+++ Portifólio Java/Project_1/BookApp/PagesOfBook.java
@@ -1,9 +1,20 @@
 package Project_1.BookApp;
 
+
 public class PagesOfBook {
     int numberOfPages;
     int goalsPerDay;
     double avarageCompletionTime;
+
+    
+
+    public void showsPagesOfBook(BookStatus book) {
+        System.out.println("Name of the book: " + book.getTitle());
+        System.out.println("Number of Pages: " + numberOfPages);
+        System.out.println("Goals Per Day: " + goalsPerDay);
+        System.out.println("Average Completion Time: " + avarageCompletionTime + " days");
+    }
+
     public int getNumberOfPages() {
         return numberOfPages;
     }
@@ -22,5 +33,9 @@ public class PagesOfBook {
 
     public void calculatingAvaregeCompletionTime() {
        this.avarageCompletionTime = numberOfPages / goalsPerDay;
+    }
+
+    public double getAvarageCompletionTime() {
+        return avarageCompletionTime;
     }
 }
