5e2001da6cfbd3c4be394946d90dc82e914f2613
Júlio <jnascimentowatanabe@gmail.com>
2026-08-10 14:44:40 -0300

    Adding attributes and annotations

--- Portifólio Java/SmallProjects/LocalizationProject/ApiProcess.java
+++ Portifólio Java/SmallProjects/LocalizationProject/ApiProcess.java
@@ -1,3 +1,16 @@
 public class ApiProcess {
+    // set attributes
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String localidade;
+    private String uf;
 
+
+
+    // set annotations
+    @Override
+    public String toString() {
+        return "Street: " + logradouro + "\n" + "neighborhood: " + bairro + "\n" + "location: " + localidade + " (" + uf + ")" + "\n" + "CEP: " + cep;
+    }
 }
