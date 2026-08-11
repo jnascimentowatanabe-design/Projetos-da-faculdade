cf4bd536e8678dce4cab265dd02585bfbfbd23c7
Júlio <jnascimentowatanabe@gmail.com>
2026-08-11 15:26:00 -0300

    Refactoring and setting a method for verify the CEP

--- Portifólio Java/SmallProjects/LocalizationProject/ApiProcess.java
+++ Portifólio Java/SmallProjects/LocalizationProject/ApiProcess.java
@@ -1,16 +1,27 @@
 public class ApiProcess {
-    // set attributes
+    // set attributes -----------------------------------------------------------------
     private String cep;
     private String logradouro;
     private String bairro;
     private String localidade;
     private String uf;
 
-
-
-    // set annotations
+    // set annotations -----------------------------------------------------------------
     @Override
     public String toString() {
-        return "Street: " + logradouro + "\n" + "neighborhood: " + bairro + "\n" + "location: " + localidade + " (" + uf + ")" + "\n" + "CEP: " + cep;
+        return "Street: " + logradouro + "\n" +
+                "neighborhood: " + bairro + "\n" + "location: " + localidade + " (" + uf + ")" +
+                "\n" + "CEP: " + cep;
+    }
+
+    // set methods -------------------------------------------------------------------------------
+
+    public static boolean verificationCep(String cep) {
+        if (cep.length() != 8) {
+            System.out.println("Check your CEP again or check if is available in Brazil request ");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
