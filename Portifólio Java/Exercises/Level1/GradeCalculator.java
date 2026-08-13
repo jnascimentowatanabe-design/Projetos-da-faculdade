46b8419828f706cbd37b6dbfdef4234f48bdfcd7
Júlio <jnascimentowatanabe@gmail.com>
2026-08-13 14:56:59 -0300

    level 3 - Grade Calculator

---
+++ Portifólio Java/Exercises/Level1/GradeCalculator.java
@@ -0,0 +1,25 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        //instancing object
+        Scanner sc = new Scanner(System.in);
+
+        //setting variable
+        double grade = 0.0;
+        double sum = 0.0;
+
+        //logic process
+        for(int counter = 0; counter <= 2; counter ++ ) {
+            System.out.println("Type the student grade: ");
+            grade = sc.nextDouble();
+            sum += grade;
+        }
+
+        double average = sum / 3;
+        System.out.println(average);
+
+    }
+}
